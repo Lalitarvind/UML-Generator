385cb609b35a294cfe789b046e068a1de496dcf7
Lalit Arvind <lalitarvind.balaji@gmail.com>
2025-06-13 13:29:07 -0700

    added basic login and singup component templates and a few assets

---
+++ frontend/src/pages/SignUp.tsx
@@ -0,0 +1,52 @@
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+
+export default function SignUp(){ 
+    return (
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>Sign Up</CardTitle>
+            <CardAction>
+              <Button variant="link">Login</Button>
+            </CardAction>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="password">Password</Label>
+                  <Input id="password" type="password" required />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="confirm_password">Confirm Password</Label>
+                  <Input id="confirm_password" type="password" required />
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </CardFooter>
+        </Card>
+      )
+}
