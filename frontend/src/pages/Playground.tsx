9058e1a763a39bb015d21882f6642b5f6db46e38
Lalit Arvind <lalitarvind.balaji@gmail.com>
2026-06-20 23:20:11 -0500

    added drag and drop nodes state transfer in playground and made some look adjustments

--- frontend/src/pages/Playground.tsx
+++ frontend/src/pages/Playground.tsx
@@ -1,4 +1,6 @@
 // src/pages/Playground.tsx
+import { useCallback } from 'react';
+import type React from 'react';
 import {
   ReactFlow,
   MiniMap,
@@ -6,61 +8,95 @@ import {
   Background,
   ReactFlowProvider,
   BackgroundVariant,
+  useReactFlow,
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 import PlaygroundHeader from '@/components/PlaygroundHeader';
 import UtilitySidebar from '@/components/UtilitySidebar';
 import ChatbotPanel from '@/components/ChatbotPanel';
-import { useDispatch, useSelector } from 'react-redux';
-import { reactFlowActions, type RFState } from '@/store/flow-slice';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { reactFlowActions } from '@/store/flow-slice';
 import ActorNode from '@/components/NodeTypes/ActorNode';
 import SystemBoundaryNode from '@/components/NodeTypes/SystemBoundaryNode';
 import UseCaseNode from '@/components/NodeTypes/UseCaseNode';
 import IncludeExcludeEdge from '@/components/EdgeTypes/IncludeExcludeEdge';
+import { SidebarProvider } from '@/components/ui/sidebar';
 
-export default function Playground() {
-  const nodes = useSelector((state:RFState) => state.nodes)
-  const edges = useSelector((state:RFState) => state.edges)
-  
-  const dispatch = useDispatch()
-  
-  const nodeTypes = {
-    actorNode: ActorNode, 
-    systemBoundaryNode: SystemBoundaryNode, 
-    useCaseNode: UseCaseNode
-  }
-  const edgeTypes = {
-    includeExcludeEdge: IncludeExcludeEdge
-  }
+const nodeTypes = {
+  actorNode: ActorNode,
+  systemBoundaryNode: SystemBoundaryNode,
+  useCaseNode: UseCaseNode,
+};
+
+const edgeTypes = {
+  includeExcludeEdge: IncludeExcludeEdge,
+};
+
+// Inner component so useReactFlow() is inside ReactFlowProvider
+function FlowCanvas() {
+  const { screenToFlowPosition } = useReactFlow();
+  const dispatch = useAppDispatch();
+  const nodes = useAppSelector((state) => state.flow.nodes);
+  const edges = useAppSelector((state) => state.flow.edges);
+
+  const onDragOver = useCallback((event: React.DragEvent) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+  }, []);
+
+  const onDrop = useCallback(
+    (event: React.DragEvent) => {
+      event.preventDefault();
+      const nodeType = event.dataTransfer.getData('application/reactflow-nodetype');
+      if (!nodeType) return;
+      const position = screenToFlowPosition({ x: event.clientX, y: event.clientY });
+      dispatch(reactFlowActions.addNode({ nodeType, position }));
+    },
+    [screenToFlowPosition, dispatch]
+  );
 
   return (
-    <div className="flex flex-col min-h-screen bg-white text-black">
+    <section
+      className="flex-1 bg-gray-50 overflow-hidden"
+      onDrop={onDrop}
+      onDragOver={onDragOver}
+    >
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        nodeTypes={nodeTypes}
+        edgeTypes={edgeTypes}
+        onNodesChange={(changes) => dispatch(reactFlowActions.onNodesChange(changes))}
+        onEdgesChange={(changes) => dispatch(reactFlowActions.onEdgesChange(changes))}
+        onConnect={(connection) => dispatch(reactFlowActions.onConnect(connection))}
+        fitView
+      >
+        <Controls />
+        <MiniMap />
+        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+      </ReactFlow>
+    </section>
+  );
+}
 
-      <PlaygroundHeader/>
-      {/* Content */}
-      <main className="flex flex-1">
-        <UtilitySidebar/>
-        {/* Canvas Area */}
-        <section className="flex-1 bg-gray-50 p-6 overflow-auto">
-          <ReactFlowProvider>
-            <ReactFlow
-              nodes={nodes}
-              edges={edges}
-              nodeTypes={nodeTypes}
-              edgeTypes={edgeTypes}
-              onNodesChange={(changes)=>dispatch(reactFlowActions.onNodesChange(changes))}
-              onEdgesChange={(changes)=>dispatch(reactFlowActions.onEdgesChange(changes))}
-              onConnect={(changes)=>dispatch(reactFlowActions.onConnect(changes))}
-              fitView
-            >
-              <Controls />
-              <MiniMap />
-              <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
-            </ReactFlow>
-          </ReactFlowProvider>
-        </section>
-        <ChatbotPanel/>
-      </main>
+export default function Playground() {
+  return (
+    <div className="flex flex-col min-h-screen bg-white text-black">
+      <PlaygroundHeader />
+      <SidebarProvider
+        style={{
+          '--sidebar-width': '20rem',
+          '--sidebar-width-mobile': '20rem',
+        } as React.CSSProperties}
+      >
+        <ReactFlowProvider>
+          <main className="flex flex-1">
+            <UtilitySidebar />
+            <FlowCanvas />
+            <ChatbotPanel />
+          </main>
+        </ReactFlowProvider>
+      </SidebarProvider>
     </div>
   );
 }
